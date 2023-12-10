6a664474fd2981fc10f33e317a5f2f34caebbe4e
gians <giansluca@gmail.com>
2023-12-10 23:52:43 +0100

    implemented transfer function

--- contracts/ZombieOwnership.sol
+++ contracts/ZombieOwnership.sol
@@ -7,6 +7,8 @@ import "./ERC721.sol";
 // import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+    mapping(uint => address) zombieApprovals;
+
     function balanceOf(address _owner) external view override returns (uint256) {
         return ownerZombieCount[_owner];
     }
@@ -16,10 +18,19 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     }
 
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable override {
-        // 3.
+        require(zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+        _transfer(_from, _to, _tokenId);
     }
 
     function approve(address _approved, uint256 _tokenId) external payable override {
         // 4.
+    }
+
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        ownerZombieCount[_to]++;
+        ownerZombieCount[_from]--;
+        zombieToOwner[_tokenId] = _to;
+
+        emit Transfer(_from, _to, _tokenId);
     }
 }
